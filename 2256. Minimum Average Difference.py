bc957c1bae762dba44866d2005c08a8260e78872
Sanskruti Dube <79304885+sanskrutidube@users.noreply.github.com>
2022-12-05 00:35:07 +0530

    Update 2256. Minimum Average Difference.py

--- 2256. Minimum Average Difference.py
+++ 2256. Minimum Average Difference.py
@@ -17,3 +17,25 @@ class Solution:
                 res = i
         
         return res
+   ---------------------------------------------------------------------------
+class Solution:
+    def minimumAverageDifference(self, a: List[int]) -> int:
+        l=0
+        r=sum(a)
+        z=100001
+        y=0
+        n=len(a)
+        
+        for i in range(n-1):
+            l+=a[i]
+            r-=a[i]
+        
+            d=abs((l//(i+1))-(r//(n-i-1)))
+            if d<z:
+                z=d
+                y=i
+        
+        if sum(a)//n<z:
+            y=n-1
+        
+        return y
