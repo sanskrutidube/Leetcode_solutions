3117c8627e1bb3c3dc9486af713a4e2ca6aa7b54
Sanskruti Dube <79304885+sanskrutidube@users.noreply.github.com>
2022-12-08 01:19:31 +0530

    Update 938. Range Sum of BST.py

--- 938. Range Sum of BST.py
+++ 938. Range Sum of BST.py
@@ -18,4 +18,45 @@ class Solution:
         return (self.sum)
         
 ----------------------------------------------------------------------------------
+def rangeSumBST(self, root: TreeNode, L: int, R: int) -> int:
+        stk, sum = [root], 0
+        while stk:
+            node = stk.pop()
+            if node:
+                if node.val > L:
+                    stk.append(node.left)    
+                if node.val < R:
+                    stk.append(node.right)
+                if L <= node.val <= R:
+                    sum += node.val    
+        return sum
+-------------------------------------------------------------------------------------
+def rangeSumBST(self, root: TreeNode, L: int, R: int) -> int:
+        if not root:
+            return 0
+        sum = 0
+        if root.val > L:
+            sum += self.rangeSumBST(root.left, L, R)
+        if root.val < R:
+            sum += self.rangeSumBST(root.right, L, R)
+        if L <= root.val <= R:
+            sum += root.val     
+        return sum
+--------------------------------------------------------------------------------------
 
+def rangeSumBST(self, root: TreeNode, L: int, R: int) -> int:
+        if not root:
+            return 0
+        return self.rangeSumBST(root.left, L, R) + \
+                self.rangeSumBST(root.right, L, R) + \
+                (root.val if L <= root.val <= R else 0)
+--------------------------------------------------------------------------------------
+
+def rangeSumBST(self, root: TreeNode, L: int, R: int) -> int:
+        if not root:
+            return 0
+        elif root.val < L:
+            return self.rangeSumBST(root.right, L, R)
+        elif root.val > R:
+            return self.rangeSumBST(root.left, L, R)
+        return root.val + self.rangeSumBST(root.left, L, R) + self.rangeSumBST(root.right, L, R)
