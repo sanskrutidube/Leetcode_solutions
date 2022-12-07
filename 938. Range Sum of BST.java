68805594604c7ff310b9098118ab6e6695496eed
Sanskruti Dube <79304885+sanskrutidube@users.noreply.github.com>
2022-12-08 01:16:43 +0530

    Create 938. Range Sum of BST.java

---
+++ 938. Range Sum of BST.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        if (root == null) return 0; // base case.
+        if (root.val < L) return rangeSumBST(root.right, L, R); // left branch excluded.
+        if (root.val > R) return rangeSumBST(root.left, L, R); // right branch excluded.
+        return root.val + rangeSumBST(root.right, L, R) + rangeSumBST(root.left, L, R); // count in both children.
+}}
+
+---------------------------------------------------------------------------------------------------------------------------
+ public int rangeSumBST(TreeNode root, int L, int R) {
+        Stack<TreeNode> stk = new Stack<>();
+        stk.push(root);
+        int sum = 0;
+        while (!stk.isEmpty()) {
+            TreeNode n = stk.pop();
+            if (n == null) { continue; }
+            if (n.val > L) { stk.push(n.left); } // left child is a possible candidate.
+            if (n.val < R) { stk.push(n.right); } // right child is a possible candidate.
+            if (L <= n.val && n.val <= R) { sum += n.val; }
+        }
+        return sum;
+    } 
+
+              
+ ---------------------------------------------------------------------------------------------------------------------------
+              
+ public int rangeSumBST(TreeNode root, int L, int R) {
+        if (root == null) return 0; // base case.
+        return (L <= root.val && root.val <= R ? root.val : 0) + rangeSumBST(root.right, L, R) + rangeSumBST(root.left, L, R);
+-------------------------------------------------------------------------------------------------------------------------------
+  public int rangeSumBST(TreeNode root, int L, int R) {
+        if (root == null) { return 0; }
+        int sum = 0;
+        if (root.val > L) { sum += rangeSumBST(root.left, L, R); } // left child is a possible candidate.
+        if (root.val < R) { sum += rangeSumBST(root.right, L, R); } // right child is a possible candidate.
+        if (root.val >= L && root.val <= R) { sum += root.val; } // count root in.
+        return sum;
+    }
