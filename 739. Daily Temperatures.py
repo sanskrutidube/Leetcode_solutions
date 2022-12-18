a07a9acef20f68d530950419e0dfd5d0b46baaf3
Sanskruti Dube <79304885+sanskrutidube@users.noreply.github.com>
2022-12-19 00:06:16 +0530

    Update 739. Daily Temperatures.py

--- 739. Daily Temperatures.py
+++ 739. Daily Temperatures.py
@@ -8,4 +8,15 @@ class Solution:
                 ans[cur] = i - cur
             stack.append(i)
         return ans
-       
+-------------------------------------------------------
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        l = len(temperatures)
+        result = [0] * l
+        for i in range(l-1):
+            for j in range(i+1, l):
+                if temperatures[j] > temperatures[i]:
+                    result[i] = j-i
+                    break
+        return result
