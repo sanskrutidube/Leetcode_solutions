4ad754339a3c3d3ddb9864e7ea8e75cbc8510375
Sanskruti Dube <79304885+sanskrutidube@users.noreply.github.com>
2022-12-28 23:42:34 +0530

    Update 41. First Missing Positive.py

--- 41. First Missing Positive.py
+++ 41. First Missing Positive.py
@@ -5,3 +5,13 @@ class Solution:
         while i in unique:
             i += 1
         return i
+------------------------------------------------------------------
+
+class Solution:
+   def firstMissingPositive(self, nums):
+       nums.sort()
+       res = 1
+       for num in nums:
+           if num == res:
+               res += 1
+       return res
