881cb16763c25e01246274e0937a28f41eea5737
Sanskruti Dube <79304885+sanskrutidube@users.noreply.github.com>
2023-01-02 20:27:31 +0530

    Create 709. To Lower Case.java

---
+++ 709. To Lower Case.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String toLowerCase(String s) {
+       StringBuilder str = new StringBuilder();
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)>=65 && s.charAt(i)<=90)
+            {
+                str.append((char)(s.charAt(i)+32));
+            }
+            else
+            {
+                str.append(s.charAt(i));
+            }
+        }
+        return str.toString();  
+    }
+}
+
+-----------------------------------------------------------
+
+class Solution {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
