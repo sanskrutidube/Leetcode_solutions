5efd9701e6ca3602eae67786c2e41eef370f6387
Sanskruti Dube <79304885+sanskrutidube@users.noreply.github.com>
2023-01-18 23:02:13 +0530

    Create 90. Subsets II.py

---
+++ 90. Subsets II.py
@@ -0,0 +1,10 @@
+class Solution:
+    def subsetsWithDup(self, S):
+        res = [[]]
+        S.sort()
+        for i in range(len(S)):
+            if i == 0 or S[i] != S[i - 1]:
+                l = len(res)
+            for j in range(len(res) - l, len(res)):
+                res.append(res[j] + [S[i]])
+        return res
