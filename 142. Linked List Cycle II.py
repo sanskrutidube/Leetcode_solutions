7ec5566045899cc9fbf60e0e73ade8a57239f3bf
Sanskruti Dube <79304885+sanskrutidube@users.noreply.github.com>
2023-04-15 01:15:45 +0530

    Create 142. Linked List Cycle II.py

---
+++ 142. Linked List Cycle II.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+#Floyd's cycle finding algorithm(PLA class)
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = head
+        fast= head
+        while fast and fast.next:
+            fast= fast.next.next
+            slow= slow.next
+            if slow==fast:
+                slow = head
+                while slow!=fast:
+                    fast=fast.next
+                    slow=slow.next
+                return slow
+        return None
+        
