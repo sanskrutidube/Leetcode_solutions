17151804ee7932f3550cc51b0256d09f9c2dfc08
Sanskruti Dube <79304885+sanskrutidube@users.noreply.github.com>
2023-07-13 00:25:18 +0530

    Create 12. Integer to Roman.py

---
+++ 12. Integer to Roman.py
@@ -0,0 +1,21 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+      
+        standard = {
+            1: "I",
+            5: "V",    4: "IV",
+            10: "X",   9: "IX",
+            50: "L",   40: "XL",
+            100: "C",  90: "XC",
+            500: "D",  400: "CD",
+            1000: "M", 900: "CM",
+        }
+        
+        number = ''
+        
+        
+        for i in [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]:
+            while i <= num:
+                number += standard[i]
+                num-=i
+        return number
